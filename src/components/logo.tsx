f008ccb7b7b2f62ddd5c850e80e32dcd989d9f27
Mahi Gupta <guptamahi0207@gmail.com>
2025-08-30 07:11:56 +0000

    in popzy remove o and add a popcorn

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,12 +1,16 @@
+
 import { cn } from '@/lib/utils';
+import { Popcorn } from 'lucide-react';
 import Link from 'next/link';
 
 const Logo = ({ className }: { className?: string }) => {
   return (
     <Link href="/" className={cn('flex items-center', className)}>
-      <span className="font-bold uppercase text-3xl tracking-wider text-foreground">
-        Popzy
-      </span>
+      <div className="font-bold uppercase text-3xl tracking-wider text-foreground flex items-center">
+        <span>P</span>
+        <Popcorn className="w-8 h-8 mx-[-2px] text-primary" />
+        <span>PZY</span>
+      </div>
     </Link>
   );
 };
