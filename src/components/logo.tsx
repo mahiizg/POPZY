7a1e541e9ab99865ae4ee3a466909cc3150ca231
Mahi Gupta <guptamahi0207@gmail.com>
2025-08-30 07:16:28 +0000

    bucket smaller

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -9,21 +9,21 @@ const PopcornIcon = (props: React.SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
   >
     {/* Bucket */}
-    <path d="M30 90 L20 20 H 80 L 70 90 Z" fill="white" stroke="gray" strokeWidth="3"/>
+    <path d="M35 80 L30 40 H 70 L 65 80 Z" fill="white" stroke="gray" strokeWidth="3"/>
     
     {/* Red Stripes */}
-    <path d="M30 90 L20 20 H 30 L 38 90 Z" fill="#D94A4A"/>
-    <path d="M50 20 L50 90" stroke="#D94A4A" strokeWidth="8"/>
-    <path d="M70 90 L80 20 H 70 L 62 90 Z" fill="#D94A4A"/>
+    <path d="M35 80 L30 40 H 40 L 43 80 Z" fill="#D94A4A"/>
+    <path d="M50 40 L50 80" stroke="#D94A4A" strokeWidth="6"/>
+    <path d="M65 80 L70 40 H 60 L 57 80 Z" fill="#D94A4A"/>
 
     {/* Popcorn */}
     <g stroke="orange" strokeWidth="2" fill="#FFFBEA">
-        <circle cx="50" cy="20" r="10" />
-        <circle cx="40" cy="25" r="12" />
-        <circle cx="60" cy="25" r="12" />
-        <circle cx="35" cy="40" r="10" />
-        <circle cx="65" cy="40" r="10" />
-        <circle cx="50" cy="35" r="12" />
+        <circle cx="50" cy="30" r="10" />
+        <circle cx="40" cy="35" r="12" />
+        <circle cx="60" cy="35" r="12" />
+        <circle cx="35" cy="45" r="10" />
+        <circle cx="65" cy="45" r="10" />
+        <circle cx="50" cy="40" r="12" />
     </g>
   </svg>
 );
